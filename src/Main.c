5671494cfa5bd133eac90d74bd3d5192cbae9fe6
lailsonzw <franciscolailsondealmeida0p9@gmail.com>
2024-09-25 22:51:24 -0300

    FEAT: Menu add in main function

--- src/Main.c
+++ src/Main.c
@@ -16,7 +16,71 @@ typedef struct No {
     struct No *direita;
 } No;
 
+//Função para criar um nó
+No *criarNo(Fruta fruta) {
+    No *novo = (No *) malloc(sizeof(No));
+    novo->fruta = fruta;
+    novo->esquerda = NULL;
+    novo->direita = NULL;
+    return novo;
+}
+
+//Função para inserir um nó na arvore
+void inserir(No **raiz, Fruta fruta) {
+    //Se a raiz for nula, o nó é inserido na raiz
+    if (*raiz == NULL) {
+        *raiz = criarNo(fruta);
+    } 
+    //Verificando se a fruta já existe na arvore
+    else if (strcmp(fruta.nome, (*raiz)->fruta.nome) == 0) {
+        (*raiz)->fruta = fruta;
+    }
+
+    //Se o nome da fruta for menor que o nome da raiz, a função é chamada recursivamente para a esquerda
+    else if (strcmp(fruta.nome, (*raiz)->fruta.nome) < 0) {
+        inserir(&(*raiz)->esquerda, fruta);
+    }
+    //Se o nome da fruta for maior que o nome da raiz, a função é chamada recursivamente para a direita
+    else {
+        inserir(&(*raiz)->direita, fruta);
+    }
+}
 
 int main(){
+   
+    int opcao;
+    
+
+    while (1) {
+        printf("1. Inserir fruta\n");
+        printf("2. Remover fruta\n");
+        printf("3. Buscar fruta\n");
+        printf("4. Modificar fruta\n");
+        printf("5. Imprimir\n");
+        printf("6. Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+
+                break;
+            case 2:
+            
+                break;
+            case 3:
+      
+                break;
+            case 4:
+   
+                break;
+            case 5:
+                system("clear");
+                break;
+            case 6:
+                exit(0);
+        }
+    }
+
     return 0;
 }
